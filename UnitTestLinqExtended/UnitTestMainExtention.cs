16cc7ced1baecfddccba02766f655f53f75ff295
eternaleap <eternaleap@gmail.com>
2015-12-28 23:33:43 +0300

    unit tests added

---
+++ UnitTestLinqExtended/UnitTestMainExtention.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqExtended;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestLinqExtended
+{
+    [TestClass]
+    public class UnitTestMainExtention
+    {
+        [TestMethod]
+        public void TestFilter()
+        {
+            Assert.IsTrue((new List<int>() { 1, 2, 5, 9 }).Filter(elem => elem > 4).Count() == 2);
+        }
+
+        [TestMethod]
+        public void TestContains()
+        {
+            var superSet = new List<int>() { 1, 2, 5, 9 };
+            var subSet = new List<int>() { 1, 2 };
+            Assert.IsTrue(superSet.Contains(subSet) && !superSet.Contains(new List<int> { 8, 2, 9 }));
+        }
+    }
+}
